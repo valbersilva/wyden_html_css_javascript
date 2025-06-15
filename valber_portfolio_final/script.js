2ade18ea586486c2407bd211e4a275c1880eb683
valbersilva <valber.l.p.silva@gmail.com>
2025-06-14 22:03:07 -0300

    projeto individual

---
+++ valber_portfolio_final/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botao = document.getElementById('agradecimentoBtn');
+    if (botao) {
+        botao.addEventListener('click', function() {
+            alert('Obrigado por visitar meu portfólio!');
+        });
+    }
+});
